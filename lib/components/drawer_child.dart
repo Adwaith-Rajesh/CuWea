93301f7990cbd7346f9fb3d9b69d33fdd1b5b9ff
Adwaith Rajesh <adwaithrajesh3180@gmail.com>
2023-11-15 16:14:59 +0530

    Added drawer

--- lib/components/drawer_child.dart
+++ lib/components/drawer_child.dart
@@ -1,8 +1,31 @@
 import 'package:cuwea/components/cuwea_logo.dart';
+import 'package:cuwea/components/cw_text.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class DrawerChild extends StatelessWidget {
   const DrawerChild({super.key});
+
+  Future<void> launchWeatherApiSite() async {
+    await launchUrl(Uri.parse('https://www.weatherapi.com/'),
+        mode: LaunchMode.externalApplication);
+  }
+
+  Future<void> launchOpenWeatherApiSite() async {
+    await launchUrl(Uri.parse('https://openweathermap.org/'),
+        mode: LaunchMode.externalApplication);
+  }
+
+  Future<void> launchGitHubPage() async {
+    await launchUrl(
+        Uri.parse('https://github.com/Adwaith-Rajesh/CuWea/tree/master'),
+        mode: LaunchMode.externalApplication);
+  }
+
+  Future<void> launchSponsorPage() async {
+    await launchUrl(Uri.parse('https://github.com/sponsors/Adwaith-Rajesh'),
+        mode: LaunchMode.externalApplication);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,10 +40,153 @@ class DrawerChild extends StatelessWidget {
           ],
         ),
       ),
-      child: Column(
+      child: ListView(
         children: [
-          DrawerHeader(
-            child: CuWeaLogo(),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  const DrawerHeader(
+                    child: CuWeaLogo(),
+                  ),
+                  Divider(
+                    color: Colors.grey[400],
+                  ),
+                  const CWText(
+                    text: 'Credits',
+                    fontSize: 12,
+                    fontWeight: FontWeight.normal,
+                  ),
+                  const SizedBox(height: 15),
+                  GestureDetector(
+                    onTap: () {
+                      launchWeatherApiSite();
+                    },
+                    child: Image.asset(
+                      'assets/images/weatherapi_logo.webp',
+                    ),
+                  ),
+                  const SizedBox(height: 25),
+                  GestureDetector(
+                    onTap: () {
+                      launchOpenWeatherApiSite();
+                    },
+                    child: Image.asset(
+                      'assets/images/owm_logo.png',
+                      width: 256,
+                      height: 50,
+                    ),
+                  ),
+                  const SizedBox(height: 25),
+                  Divider(
+                    color: Colors.grey[400],
+                  ),
+                  const CWText(
+                    text: 'Like what you see?',
+                    fontSize: 12,
+                    fontWeight: FontWeight.normal,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      launchGitHubPage();
+                    },
+                    child: Container(
+                      margin: const EdgeInsets.all(15),
+                      padding: const EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.grey.shade800,
+                        ),
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      child: Column(
+                        children: [
+                          const CWText(
+                            text: 'Give us a star on GitHub',
+                            fontSize: 12,
+                            fontWeight: FontWeight.normal,
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Image.asset(
+                                'assets/images/github-logo.png',
+                                width: 50,
+                                height: 50,
+                              ),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              const CWText(
+                                  text: 'CuWea',
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold)
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      launchSponsorPage();
+                    },
+                    child: Container(
+                      margin: const EdgeInsets.only(
+                        left: 15,
+                        right: 15,
+                      ),
+                      padding: const EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.grey.shade800,
+                        ),
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      child: const Column(
+                        children: [
+                          CWText(
+                            text: 'Or, Sponsor Us',
+                            fontSize: 12,
+                            fontWeight: FontWeight.normal,
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.favorite_outline,
+                                color: Colors.pink,
+                                size: 50,
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              CWText(
+                                  text: 'CuWea',
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold)
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Divider(
+                    color: Colors.grey[400],
+                  ),
+                ],
+              ),
+            ],
           ),
         ],
       ),
